9baa6376ee7841ef99b2c9efd1b0d152efb9ca49
Robert Kovacsics <kovirobi@gmail.com>
2019-10-11 15:13:10 +0100

    Moved system state version into *-config

--- base-configuration.nix
+++ base-configuration.nix
@@ -97,10 +97,4 @@
 
   networking.firewall.allowedTCPPorts = [ ];
   networking.firewall.allowedUDPPorts = [ ];
-
-  # This value determines the NixOS release with which your system is to be
-  # compatible, in order to avoid breaking some software such as database
-  # servers. You should change this only after NixOS release notes say you
-  # should.
-  system.stateVersion = "19.03"; # Did you read the comment?
 }
--- cl.cam.ac.uk.nix
+++ cl.cam.ac.uk.nix
@@ -58,4 +58,10 @@
   services.logind.extraConfig = "HandlePowerKey=suspend";
 
   services.xserver.videoDrivers = [ "intel" "nouveau" "vesa" "modesetting" ];
+
+  # This value determines the NixOS release with which your system is to be
+  # compatible, in order to avoid breaking some software such as database
+  # servers. You should change this only after NixOS release notes say you
+  # should.
+  system.stateVersion = "19.03"; # Did you read the comment?
 }
