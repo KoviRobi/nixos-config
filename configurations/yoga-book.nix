df06e58cec0501373236be46f9c335e3a3c582ea
Robert Kovacsics <robert.kovacsics@cambridgeconsultants.com>
2022-04-06 11:04:22 +0100

    Move yoga-book eink driver to yoga-book config

--- configurations/yoga-book.nix
+++ configurations/yoga-book.nix
@@ -22,6 +22,16 @@
     "pinctrl_sunrisepoint" # For booting off SD card
     "eink"
   ];
+  boot.kernelPackages = pkgs.linuxPackages.extend (self: super: {
+    yogabook-c930-eink-driver = self.callPackage
+      (pkgs.fetchgit {
+        url = "https://github.com/KoviRobi/yogabook-c930-linux-eink-driver";
+        rev = "d990b6295034436c3ac8aa451526eb2864553d9b";
+        sha256 = "0ysz0y0bxr17fpzys2k8a0frzb681g4qvdnm4i9arcqc38b61xha";
+        fetchSubmodules = false;
+      })
+      { };
+  });
   boot.extraModulePackages = [ pkgs.linuxPackages.yogabook-c930-eink-driver ];
 
   environment.systemPackages = with pkgs; [ ntfs3g ];
