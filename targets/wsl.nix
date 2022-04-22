5df025940fd937adce9c1b34b9c46b63ac043da7
Robert Kovacsics <robert.kovacsics@cambridgeconsultants.com>
2022-04-22 22:12:54 +0100

    Automatically get WSL DNS config

--- targets/wsl.nix
+++ targets/wsl.nix
@@ -12,10 +12,17 @@
     wslConf.network.generateResolvConf = "false";
     wslConf.network.hostname = config.networking.hostName;
   };
-  environment.etc."resolv.conf".enable = lib.mkForce true;
-  environment.etc."resolv.conf".text = ''
-    nameserver 1.1.1.1
-    nameserver 8.8.8.8
+
+  systemd.services."resolv.conf".serviceConfig = { PassEnvironment = "WSL_INTEROP"; };
+  systemd.services."resolv.conf".wantedBy = [ "default.target" ];
+  systemd.services."resolv.conf".script = ''
+    /mnt/c/windows/System32/WindowsPowerShell/v1.0/powershell.exe \
+      -Command "(Get-DnsClientServerAddress \
+                      -InterfaceAlias Ethernet \
+                      -AddressFamily IPv4 \
+                ).ServerAddresses" |
+      ${pkgs.dos2unix}/bin/dos2unix |
+      ${pkgs.gnused}/bin/sed 's/^/nameserver /' > /etc/resolv.conf
   '';
   users.users.default-user.extraGroups = [ "no-google-authenticator" ];
   services.xserver.dpi = 100;
