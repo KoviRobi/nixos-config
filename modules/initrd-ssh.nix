17c4266d7a6c3290eb058158aafe6753915557ae
Robert Kovacsics <robert.kovacsics@cambridgeconsultants.com>
2022-06-17 13:42:36 +0100

    Update initrd ssh

--- modules/initrd-ssh.nix
+++ modules/initrd-ssh.nix
@@ -7,7 +7,7 @@
   boot.initrd.network.enable = true;
   boot.initrd.network.ssh.enable = true;
   boot.initrd.network.ssh.authorizedKeys = [
-    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILaP5itEgT3/sUeln+cvaIfBznr4e17cjXgCP6X63GYD rmk@cc-vm-nixos-a;ed25519"
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHfojmpABobr3CEgN3eCu3mEqpr3oAs2h5NfY1ZtppIK ccl\rmk@RMKW10L2"
   ];
   boot.initrd.network.ssh.hostKeys = [ "/etc/secrets/initrd/ssh_host_ed25519_key" ];
   # Make sure ordinary users can't access initrd SSH host key
