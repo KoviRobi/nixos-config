b1665a759585ad9b525d201b6af79a796b0944ab
Kovacsics Robert <kovirobi@gmail.com>
2022-10-28 17:12:17 +0100

    Remove no longer existing rpfilter rules

--- configurations/pc.nix
+++ configurations/pc.nix
@@ -40,21 +40,6 @@
     1900 # MiniDLNA
   ];
 
-  # WireGuard
-  networking.firewall = {
-    # if packets are still dropped, they will show up in dmesg
-    logReversePathDrops = true;
-    # wireguard trips rpfilter up
-    extraCommands = ''
-      ip46tables -t raw -I nixos-fw-rpfilter -p udp -m udp --sport 51820 -j RETURN
-      ip46tables -t raw -I nixos-fw-rpfilter -p udp -m udp --dport 51820 -j RETURN
-    '';
-    extraStopCommands = ''
-      ip46tables -t raw -D nixos-fw-rpfilter -p udp -m udp --sport 51820 -j RETURN || true
-      ip46tables -t raw -D nixos-fw-rpfilter -p udp -m udp --dport 51820 -j RETURN || true
-    '';
-  };
-
   networking.networkmanager.insertNameservers = [ "1.1.1.1" "1.1.0.0" ];
 
   services.xserver =
