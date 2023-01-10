d4735e93077156a04c0833c7ec5442fc9a5d0fa3
Robert Kovacsics <robert.kovacsics@cambridgeconsultants.com>
2023-01-10 21:23:11 +0000

    Add ST desktopentry patch

--- overlays/mypkgs.nix
+++ overlays/mypkgs.nix
@@ -13,6 +13,7 @@ self: super:
       ../patches/st-0.8.5-font2.patch
       ../patches/st-0.8.5-worddelimiters.patch
       ../patches/st-0.8.5-netwmicon-v2.patch
+      ../patches/st-0.8.5-desktopentry.patch
     ];
   }).overrideDerivation (oldDrv: {
     ICONSRC = "${self.paper-icon-theme}/share/icons/Paper/32x32/apps/utilities-terminal-alt.png";
