13fc727ccb98237e6340d3047d620e21f13d65fa
Kovacsics Robert <kovirobi@gmail.com>
2023-02-27 23:03:48 +0000

    Add dwarffs
    • Added input 'dwarffs':
        'github:edolstra/dwarffs/1f850df9c932acb95da2f31b576a8f6c7c188376' (2022-09-06)
    • Added input 'dwarffs/nix':
        'github:NixOS/nix/586fa707fca207dbd12e49800691390249bdcd03' (2022-06-24)
    • Added input 'dwarffs/nix/lowdown-src':
        'github:kristapsdz/lowdown/d2c2b44ff6c27b936ec27358a2653caaef8f73b8' (2021-10-06)
    • Added input 'dwarffs/nix/nixpkgs':
        'github:NixOS/nixpkgs/2fa57ed190fd6c7c746319444f34b5917666e5c1' (2022-05-31)
    • Added input 'dwarffs/nix/nixpkgs-regression':
        'github:NixOS/nixpkgs/215d4d0fd80ca5163643b03a33fde804a29cc1e2' (2022-01-24)
    • Added input 'dwarffs/nixpkgs':
        follows 'dwarffs/nix/nixpkgs'

--- flake.nix
+++ flake.nix
@@ -23,206 +23,221 @@
   inputs.deploy-rs.inputs.nixpkgs.follows = "nixpkgs";
   inputs.deploy-rs.inputs.flake-compat.follows = "flake-compat";
 
-  outputs = { self, nixpkgs, utils, home-manager, pye-menu, flake-compat, flake-registry, NixOS-WSL, deploy-rs }: {
+  inputs.dwarffs.url = "github:edolstra/dwarffs";
 
-    overlays =
-      let
-        inherit (builtins) attrValues elemAt filter listToAttrs map mapAttrs match readDir;
-        get_nix_name = name:
-          let m = match "^(.*)\.nix$" name; in
-          if m == null then null else elemAt m 0;
-        contents = attrValues
-          (mapAttrs
-            (name: type: { inherit name type; base = get_nix_name name; })
-            (readDir ./overlays));
-        nix_or_dirs = filter (attrs: attrs.base != null || attrs.type == "directory") contents;
-        imported = map
-          (attrs: {
-            name = if attrs.type == "directory" then attrs.name else attrs.base;
-            value = import (./overlays + ("/" + attrs.name));
-          })
-          nix_or_dirs;
-      in
-      listToAttrs imported;
+  outputs =
+    { self
+    , nixpkgs
+    , utils
+    , home-manager
+    , pye-menu
+    , flake-compat
+    , flake-registry
+    , NixOS-WSL
+    , deploy-rs
+    , dwarffs
+    }: {
 
-    homeModules.simple = [
-      ./home/direnv.nix
-      ./home/git.nix
-      ./home/shell.nix
-      ./home/solarized.nix
-      ./home/tmux.nix
-    ];
+      overlays =
+        let
+          inherit (builtins) attrValues elemAt filter listToAttrs map mapAttrs match readDir;
+          get_nix_name = name:
+            let m = match "^(.*)\.nix$" name; in
+            if m == null then null else elemAt m 0;
+          contents = attrValues
+            (mapAttrs
+              (name: type: { inherit name type; base = get_nix_name name; })
+              (readDir ./overlays));
+          nix_or_dirs = filter (attrs: attrs.base != null || attrs.type == "directory") contents;
+          imported = map
+            (attrs: {
+              name = if attrs.type == "directory" then attrs.name else attrs.base;
+              value = import (./overlays + ("/" + attrs.name));
+            })
+            nix_or_dirs;
+        in
+        listToAttrs imported;
 
-    packages = utils.lib.eachDefaultSystemMap (system:
-      {
-        homeConfigurations.simple = home-manager.lib.homeManagerConfiguration
-          {
-            modules = self.homeModules.simple ++ [
+      homeModules.simple = [
+        ./home/direnv.nix
+        ./home/git.nix
+        ./home/shell.nix
+        ./home/solarized.nix
+        ./home/tmux.nix
+      ];
 
-              {
-                home.username = "rmk";
-                home.homeDirectory = "/hom/rmk";
-                home.stateVersion = "22.11";
-              }
-            ];
-            pkgs = import nixpkgs {
-              inherit system;
-              overlays = builtins.attrValues self.overlays;
-            };
-          };
-
-        netboot =
-          let
-            system = "x86_64-linux";
-            pkgs = import nixpkgs { inherit system; };
-            netboot-system = self.nixosConfigurations.netboot;
-            kernel-cmdline = [ "init=${toplevel}/init" ] ++ netboot-system.config.boot.kernelParams;
-            inherit (netboot-system.config.system.build) kernel netbootRamdisk toplevel;
-          in
-          pkgs.writeShellApplication
+      packages = utils.lib.eachDefaultSystemMap (system:
+        {
+          homeConfigurations.simple = home-manager.lib.homeManagerConfiguration
             {
-              name = "netboot";
-              text = ''
-                cat <<EOF
-                Don't forget to open the following ports in the firewall:
-                UDP: 67 69 4011
-                TCP: 64172
+              modules = self.homeModules.simple ++ [
 
-                This can be done via
-
-                    sudo iptables -I nixos-fw 1 -i enp4s0 -p udp -m udp --dport 67    -j nixos-fw-accept
-                    sudo iptables -I nixos-fw 2 -i enp4s0 -p udp -m udp --dport 69    -j nixos-fw-accept
-                    sudo iptables -I nixos-fw 3 -i enp4s0 -p udp -m udp --dport 4011  -j nixos-fw-accept
-                    sudo iptables -I nixos-fw 4 -i enp4s0 -p tcp -m tcp --dport 64172 -j nixos-fw-accept
-
-                (change enp4s0 to the interface you are using).
-
-                And once you are done, closed via
-
-                    sudo iptables -D nixos-fw -i enp4s0 -p udp -m udp --dport 67    -j nixos-fw-accept
-                    sudo iptables -D nixos-fw -i enp4s0 -p udp -m udp --dport 69    -j nixos-fw-accept
-                    sudo iptables -D nixos-fw -i enp4s0 -p udp -m udp --dport 4011  -j nixos-fw-accept
-                    sudo iptables -D nixos-fw -i enp4s0 -p tcp -m tcp --dport 64172 -j nixos-fw-accept
-
-                If you need to do DHCP also, consider
-
-                    sudo nix run nixpkgs\#dnsmasq -- \\
-                      --interface enp4s0 \\
-                      --dhcp-range 192.168.10.10,192.168.10.254 \\
-                      --dhcp-leasefile=dnsmasq.leases \\
-                      --no-daemon
-                EOF
-                nix run nixpkgs\#pixiecore -- \
-                  boot ${kernel}/bzImage ${netbootRamdisk}/initrd \
-                  --cmdline "${builtins.concatStringsSep " " kernel-cmdline}" \
-                  --debug --dhcp-no-bind --port 64172 --status-port 64172 \
-                  "$@"
-              '';
+                {
+                  home.username = "rmk";
+                  home.homeDirectory = "/hom/rmk";
+                  home.stateVersion = "22.11";
+                }
+              ];
+              pkgs = import nixpkgs {
+                inherit system;
+                overlays = builtins.attrValues self.overlays;
+              };
             };
 
-      });
+          netboot =
+            let
+              system = "x86_64-linux";
+              pkgs = import nixpkgs { inherit system; };
+              netboot-system = self.nixosConfigurations.netboot;
+              kernel-cmdline = [ "init=${toplevel}/init" ] ++ netboot-system.config.boot.kernelParams;
+              inherit (netboot-system.config.system.build) kernel netbootRamdisk toplevel;
+            in
+            pkgs.writeShellApplication
+              {
+                name = "netboot";
+                text = ''
+                  cat <<EOF
+                  Don't forget to open the following ports in the firewall:
+                  UDP: 67 69 4011
+                  TCP: 64172
 
-    nixpkgs = import nixpkgs { overlays = builtins.attrValues self.overlays; };
+                  This can be done via
 
-    nixosConfigurations = builtins.mapAttrs
-      (name: value:
-        nixpkgs.lib.nixosSystem rec {
+                      sudo iptables -I nixos-fw 1 -i enp4s0 -p udp -m udp --dport 67    -j nixos-fw-accept
+                      sudo iptables -I nixos-fw 2 -i enp4s0 -p udp -m udp --dport 69    -j nixos-fw-accept
+                      sudo iptables -I nixos-fw 3 -i enp4s0 -p udp -m udp --dport 4011  -j nixos-fw-accept
+                      sudo iptables -I nixos-fw 4 -i enp4s0 -p tcp -m tcp --dport 64172 -j nixos-fw-accept
+
+                  (change enp4s0 to the interface you are using).
+
+                  And once you are done, closed via
+
+                      sudo iptables -D nixos-fw -i enp4s0 -p udp -m udp --dport 67    -j nixos-fw-accept
+                      sudo iptables -D nixos-fw -i enp4s0 -p udp -m udp --dport 69    -j nixos-fw-accept
+                      sudo iptables -D nixos-fw -i enp4s0 -p udp -m udp --dport 4011  -j nixos-fw-accept
+                      sudo iptables -D nixos-fw -i enp4s0 -p tcp -m tcp --dport 64172 -j nixos-fw-accept
+
+                  If you need to do DHCP also, consider
+
+                      sudo nix run nixpkgs\#dnsmasq -- \\
+                        --interface enp4s0 \\
+                        --dhcp-range 192.168.10.10,192.168.10.254 \\
+                        --dhcp-leasefile=dnsmasq.leases \\
+                        --no-daemon
+                  EOF
+                  nix run nixpkgs\#pixiecore -- \
+                    boot ${kernel}/bzImage ${netbootRamdisk}/initrd \
+                    --cmdline "${builtins.concatStringsSep " " kernel-cmdline}" \
+                    --debug --dhcp-no-bind --port 64172 --status-port 64172 \
+                    "$@"
+                '';
+              };
+
+        });
+
+      nixpkgs = import nixpkgs { overlays = builtins.attrValues self.overlays; };
+
+      nixosConfigurations = builtins.mapAttrs
+        (name: value:
+          nixpkgs.lib.nixosSystem rec {
+            system = "x86_64-linux";
+            modules =
+              value ++ [
+                { networking.hostName = name; }
+
+                # Let 'nixos-version --json' know about the Git revision
+                # of this flake.
+                # From https://www.tweag.io/blog/2020-07-31-nixos-flakes/
+                {
+                  system.configurationRevision =
+                    if self ? rev
+                    then self.rev
+                    else throw "Refusing to build from a dirty Git tree!";
+                }
+
+                { nixpkgs.overlays = builtins.attrValues self.overlays; }
+
+                ({ config, pkgs, ... }: {
+                  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+                  # Pin nixpkgs for e.g. nix search
+                  nix.registry.nixpkgs.flake = nixpkgs;
+                  nix.registry.nixos-config.flake = self;
+
+                  # ~/.config/nix/registry.json, if you want to use a local
+                  # checkout:
+                  #
+                  #     {
+                  #       "flakes": [
+                  #         {
+                  #           "exact": true,
+                  #           "from": {
+                  #             "id": "nixpkgs",
+                  #             "type": "indirect"
+                  #           },
+                  #           "to": {
+                  #             "type": "git",
+                  #             "url": "file:///nix/pkgs"
+                  #           }
+                  #         }
+                  #       ],
+                  #       "version": 2
+                  #     }
+
+                  nix.nixPath = [
+                    "nixpkgs=${nixpkgs}"
+                    "home-manager=${home-manager}"
+                    "${nixpkgs}"
+                  ];
+
+                  environment.shellAliases.nixrepl =
+                    "nix repl --expr 'builtins.getFlake \"${self}\"'";
+                })
+
+                home-manager.nixosModule
+                {
+                  environment.systemPackages = [ home-manager.defaultPackage.${system} ];
+                  home-manager.useGlobalPkgs = true;
+                  home-manager.useUserPackages = true;
+                  home-manager.extraSpecialArgs = { inherit pye-menu; };
+                }
+
+                { environment.systemPackages = [ deploy-rs.defaultPackage.${system} ]; }
+
+                dwarffs.nixosModules.dwarffs
+              ];
+          }
+        )
+        {
+          "pc-nixos-a" = [ ./configurations/pc.nix ./targets/pc.nix ];
+          "hp-nixos-a" = [ ./configurations/hp.nix ./targets/hp.nix ];
+          "rmk-cc-pc-nixos-a" = [ ./configurations/cc-pc.nix ./targets/cc-pc.nix ];
+          "cc-wsl" = [ NixOS-WSL.nixosModules.wsl ./configurations/cc-wsl.nix ./targets/wsl.nix ];
+          "pc-wsl" = [ NixOS-WSL.nixosModules.wsl ./configurations/pc-wsl.nix ./targets/wsl.nix ];
+          "iso" = [ ./configurations/cc.nix (import ./targets/iso-image.nix { inherit self nixpkgs; }) ];
+        } // {
+        "netboot" = nixpkgs.lib.nixosSystem rec {
           system = "x86_64-linux";
-          modules =
-            value ++ [
-              { networking.hostName = name; }
+          modules = [
+            "${nixpkgs}/nixos/modules/installer/netboot/netboot.nix"
 
-              # Let 'nixos-version --json' know about the Git revision
-              # of this flake.
-              # From https://www.tweag.io/blog/2020-07-31-nixos-flakes/
-              {
-                system.configurationRevision =
-                  if self ? rev
-                  then self.rev
-                  else throw "Refusing to build from a dirty Git tree!";
-              }
+            # Profiles of this basic netboot media
+            "${nixpkgs}/nixos/modules/profiles/all-hardware.nix"
+            "${nixpkgs}/nixos/modules/profiles/installation-device.nix"
+            ({ pkgs, lib, ... }: {
+              system.extraDependencies = lib.mkForce [ ];
+              documentation.enable = lib.mkForce false;
 
-              { nixpkgs.overlays = builtins.attrValues self.overlays; }
-
-              ({ config, pkgs, ... }: {
-                nix.settings.experimental-features = [ "nix-command" "flakes" ];
-                # Pin nixpkgs for e.g. nix search
-                nix.registry.nixpkgs.flake = nixpkgs;
-                nix.registry.nixos-config.flake = self;
-
-                # ~/.config/nix/registry.json, if you want to use a local
-                # checkout:
-                #
-                #     {
-                #       "flakes": [
-                #         {
-                #           "exact": true,
-                #           "from": {
-                #             "id": "nixpkgs",
-                #             "type": "indirect"
-                #           },
-                #           "to": {
-                #             "type": "git",
-                #             "url": "file:///nix/pkgs"
-                #           }
-                #         }
-                #       ],
-                #       "version": 2
-                #     }
-
-                nix.nixPath = [
-                  "nixpkgs=${nixpkgs}"
-                  "home-manager=${home-manager}"
-                  "${nixpkgs}"
-                ];
-
-                environment.shellAliases.nixrepl =
-                  "nix repl --expr 'builtins.getFlake \"${self}\"'";
-              })
-
-              home-manager.nixosModule
-              {
-                environment.systemPackages = [ home-manager.defaultPackage.${system} ];
-                home-manager.useGlobalPkgs = true;
-                home-manager.useUserPackages = true;
-                home-manager.extraSpecialArgs = { inherit pye-menu; };
-              }
-
-              { environment.systemPackages = [ deploy-rs.defaultPackage.${system} ]; }
-            ];
-        }
-      )
-      {
-        "pc-nixos-a" = [ ./configurations/pc.nix ./targets/pc.nix ];
-        "hp-nixos-a" = [ ./configurations/hp.nix ./targets/hp.nix ];
-        "rmk-cc-pc-nixos-a" = [ ./configurations/cc-pc.nix ./targets/cc-pc.nix ];
-        "cc-wsl" = [ NixOS-WSL.nixosModules.wsl ./configurations/cc-wsl.nix ./targets/wsl.nix ];
-        "pc-wsl" = [ NixOS-WSL.nixosModules.wsl ./configurations/pc-wsl.nix ./targets/wsl.nix ];
-        "iso" = [ ./configurations/cc.nix (import ./targets/iso-image.nix { inherit self nixpkgs; }) ];
-      } // {
-      "netboot" = nixpkgs.lib.nixosSystem rec {
-        system = "x86_64-linux";
-        modules = [
-          "${nixpkgs}/nixos/modules/installer/netboot/netboot.nix"
-
-          # Profiles of this basic netboot media
-          "${nixpkgs}/nixos/modules/profiles/all-hardware.nix"
-          "${nixpkgs}/nixos/modules/profiles/installation-device.nix"
-          ({ pkgs, lib, ... }: {
-            system.extraDependencies = lib.mkForce [ ];
-            documentation.enable = lib.mkForce false;
-
-            users.users.nixos.openssh.authorizedKeys.keys = [
-              "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKG9Dg3j/KJgDbtsUSyOJBF7+bQzfDQpLo4gqDX195rJ rmk@rmk-cc-pc-nixos-a"
-              "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBxS1hIoi4jj4h00KoIfBJJX6aMF5TtdZZxBOqLRRKCH rmk35@pc-nixos-a"
-            ];
-            # To allow using custom substituters (e.g. netboot host)
-            nix.settings.trusted-users = [ "nixos" ];
-            nix.settings.flake-registry = "${flake-registry}/flake-registry.json";
-            environment.systemPackages = with pkgs; [ gitMinimal ];
-          })
-        ];
+              users.users.nixos.openssh.authorizedKeys.keys = [
+                "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKG9Dg3j/KJgDbtsUSyOJBF7+bQzfDQpLo4gqDX195rJ rmk@rmk-cc-pc-nixos-a"
+                "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBxS1hIoi4jj4h00KoIfBJJX6aMF5TtdZZxBOqLRRKCH rmk35@pc-nixos-a"
+              ];
+              # To allow using custom substituters (e.g. netboot host)
+              nix.settings.trusted-users = [ "nixos" ];
+              nix.settings.flake-registry = "${flake-registry}/flake-registry.json";
+              environment.systemPackages = with pkgs; [ gitMinimal ];
+            })
+          ];
+        };
       };
     };
-  };
 }
