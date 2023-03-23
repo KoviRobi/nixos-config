00ce5b6a60abca8527c00e85d245522b32f2c94d
Robert Kovacsics <robert.kovacsics@cambridgeconsultants.com>
2023-03-23 15:30:51 +0000

    Add Will's key to use my machine as a Nix cache

--- configurations/cc-pc.nix
+++ configurations/cc-pc.nix
@@ -59,6 +59,7 @@
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIF8kc9byAsBL3Jt1zynOKBrDjp/Uwm774ymj3DoPNVSi root@cc-wsl"
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINlxBXBCN14zoA3qmd31d/Nonaef5Cag4RKlsDlddFjJ mconway"
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINBUSeOyKZcJ3KwQZ25dTdcAA0eJ75CbfXd2ToXiqrJB root@gitlab-runner.p3651"
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDCkYPC+gZP1YuLReyD05ZJaGbo4BMcI/D8RyiQd4BNh wwykeham@wpwlx1"
   ];
   nix.settings.secret-key-files = "/etc/secrets/nix/secret-key";
   # Set modules/ssh.nix to not require authenticator key for nix-ssh
