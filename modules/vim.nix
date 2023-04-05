335cc160b018f040a49b57174eb81379a7ae4f47
Robert Kovacsics <robert.kovacsics@cambridgeconsultants.com>
2023-04-05 14:13:07 +0100

    Readd treesitter
    I removed it in commit 4f5dbf3efca145b44fe92a69963a5c14b133e8a7 as it
    seems to break whole NixOS rebuild, hopefully it's better now

--- modules/vim.nix
+++ modules/vim.nix
@@ -259,7 +259,38 @@ in
       let g:neoterm_autoinsert = 1
       autocmd BufEnter term://* startinsert
 
+      lua <<
+      require'nvim-treesitter.configs'.setup {
+        highlight = {
+          -- `false` will disable the whole extension
+          enable = true,
+          disable = { "gitcommit", "man" },
+
+          -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+          -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
+          -- Using this option may slow down your editor, and you may see some duplicate highlights.
+          -- Instead of true it can also be a list of languages
+          additional_vim_regex_highlighting = false,
+        },
+        indent = {
+          enable = true,
+        },
+        rainbow = {
+          enable = true,
+          -- Also highlight non-bracket delimiters like html tags, boolean or table: lang -> boolean
+          extended_mode = true,
+          -- Do not enable for files with more than n lines, int
+          max_file_lines = nil,
+          -- table of hex strings
+          -- colors = {},
+          -- table of colour name strings
+          -- termcolors = {}
+        },
+      }
+      .
+
       set foldmethod=expr
+      set foldexpr=nvim_treesitter#foldexpr()
       set foldlevelstart=10
       set foldcolumn=auto
     '';
@@ -302,6 +333,11 @@ in
         vim-unstack
         gitmoji-vim
         vim-nushell
+
+        nvim-treesitter.withAllGrammars
+        nvim-ts-rainbow
+        nvim-treesitter-context
+        nvim-treesitter-textobjects
       ];
 
     nixpkgs.overlays = [
