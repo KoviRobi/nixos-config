0811a560ef90f6817a53a0fc3ce341c3c5897731
Robert Kovacsics <robert.kovacsics@cambridgeconsultants.com>
2023-12-18 13:59:03 +0000

    Update nushell

--- overlays/nushell.nix
+++ overlays/nushell.nix
@@ -10,21 +10,21 @@ final: prev:
   nushell =
     prev.nushell.overrideAttrs (oldAttrs: rec {
       pname = "nushell";
-      version = "unstable-2023-09-15";
+      version = "unstable-2023-12-18";
 
       src = final.fetchFromGitHub {
         owner = "KoviRobi";
         repo = pname;
         rev = "rob";
-        sha256 = "sha256-nSHOabZ3nW1ticQHYJKhQKD0Eqkk6eUo9HPhHqxcjiw=";
+        sha256 = "sha256-d2sPjx1VFp5zDU2o+VWOrEml3TEfhbkwez8yjwarsBw=";
       };
 
       doCheck = false;
 
       cargoDeps = prev.rustPlatform.importCargoLock {
         outputHashes = {
-          "lsp-server-0.7.4" = "sha256-TEYr3dOEXBt714uKx1uEsI4pB1TkUjXazfN1Z8icyEU=";
-          # "reedline-0.25.0" = "sha256-J6AtZXoV8sRGQ75xq3ReRt3Hjz09bWLFzUnFDYCKg7s=";
+          "reedline-0.27.1" = "sha256-8vrCaldEYdI3R7pIXQ+L3KI3JHYRa3xAiFAt3EmP24A=";
+          # "lsp-server-0.7.4" = "sha256-TEYr3dOEXBt714uKx1uEsI4pB1TkUjXazfN1Z8icyEU=";
           # "uu_cp-0.0.21" = "sha256-tM8+M+6TrAL839HnvSPTe9is4fMoi4S/D0Kg/C0juK4=";
           # "uucore-0.0.21" = "sha256-tM8+M+6TrAL839HnvSPTe9is4fMoi4S/D0Kg/C0juK4=";
           # "uucore_procs-0.0.21" = "sha256-tM8+M+6TrAL839HnvSPTe9is4fMoi4S/D0Kg/C0juK4=";
