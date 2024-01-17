6ae4abff9a66bc479c8156c6b63881023452cac3
Robert Kovacsics <robert.kovacsics@cambridgeconsultants.com>
2024-01-17 13:52:06 +0000

    Update nushell

--- overlays/nushell.nix
+++ overlays/nushell.nix
@@ -16,14 +16,14 @@ final: prev:
         owner = "KoviRobi";
         repo = pname;
         rev = "rob";
-        sha256 = "sha256-mkzM8iyz7+vJczHd3rUd3dBtun/6PH2X9x+6k66svp0=";
+        sha256 = "sha256-TxBj/fPaXX1SZvXEVCBPLqYkU3N/Nig93Kp9X+wTfnk=";
       };
 
       doCheck = false;
 
       cargoDeps = prev.rustPlatform.importCargoLock {
         outputHashes = {
-          "reedline-0.27.1" = "sha256-8vrCaldEYdI3R7pIXQ+L3KI3JHYRa3xAiFAt3EmP24A=";
+          "reedline-0.28.0" = "sha256-Voc9yFpdcEE/EsxHmTO/w2I1jnXKPFg0qQiLQrx/hIM=";
           # "lsp-server-0.7.4" = "sha256-TEYr3dOEXBt714uKx1uEsI4pB1TkUjXazfN1Z8icyEU=";
           # "uu_cp-0.0.21" = "sha256-tM8+M+6TrAL839HnvSPTe9is4fMoi4S/D0Kg/C0juK4=";
           # "uucore-0.0.21" = "sha256-tM8+M+6TrAL839HnvSPTe9is4fMoi4S/D0Kg/C0juK4=";
