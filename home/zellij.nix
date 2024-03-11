97d607218abcbfc0afc544829292c96a89d15bf2
Robert Kovacsics <robertkovacsics@carallon.com>
2024-03-11 12:25:26 +0000

    Fix default zellij color-schemes overriding

--- home/zellij.nix
+++ home/zellij.nix
@@ -5,31 +5,31 @@
     settings = {
       pane_frames = false;
 
-      theme = "solarized-light";
+      theme = "solarized_light";
 
-      themes.solarized-dark.fg = "#FDF6E3";
-      themes.solarized-dark.bg = "#002B36";
-      themes.solarized-dark.black = "#EEE8D5";
-      themes.solarized-dark.white = "#073642";
-      themes.solarized-dark.red = "#DC322F";
-      themes.solarized-dark.green = "#859900";
-      themes.solarized-dark.yellow = "#B58900";
-      themes.solarized-dark.blue = "#268BD2";
-      themes.solarized-dark.magenta = "#D33682";
-      themes.solarized-dark.cyan = "#2AA198";
-      themes.solarized-dark.orange = "#CB4B16";
+      themes.solarized_dark.fg = "#FDF6E3";
+      themes.solarized_dark.bg = "#002B36";
+      themes.solarized_dark.black = "#EEE8D5";
+      themes.solarized_dark.white = "#073642";
+      themes.solarized_dark.red = "#DC322F";
+      themes.solarized_dark.green = "#859900";
+      themes.solarized_dark.yellow = "#B58900";
+      themes.solarized_dark.blue = "#268BD2";
+      themes.solarized_dark.magenta = "#D33682";
+      themes.solarized_dark.cyan = "#2AA198";
+      themes.solarized_dark.orange = "#CB4B16";
 
-      themes.solarized-light.fg = "#657B83";
-      themes.solarized-light.bg = "#FDF6E3";
-      themes.solarized-light.black = "#EEE8D5";
-      themes.solarized-light.white = "#073642";
-      themes.solarized-light.red = "#DC322F";
-      themes.solarized-light.green = "#859900";
-      themes.solarized-light.yellow = "#B58900";
-      themes.solarized-light.blue = "#268BD2";
-      themes.solarized-light.magenta = "#D33682";
-      themes.solarized-light.cyan = "#2AA198";
-      themes.solarized-light.orange = "#CB4B16";
+      themes.solarized_light.fg = "#657B83";
+      themes.solarized_light.bg = "#FDF6E3";
+      themes.solarized_light.black = "#EEE8D5";
+      themes.solarized_light.white = "#073642";
+      themes.solarized_light.red = "#DC322F";
+      themes.solarized_light.green = "#859900";
+      themes.solarized_light.yellow = "#B58900";
+      themes.solarized_light.blue = "#268BD2";
+      themes.solarized_light.magenta = "#D33682";
+      themes.solarized_light.cyan = "#2AA198";
+      themes.solarized_light.orange = "#CB4B16";
     };
   };
 }
