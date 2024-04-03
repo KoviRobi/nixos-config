f2539fa724c3fa7d30931da0925dc0ce3eedf79a
Kovacsics Robert <kovirobi@gmail.com>
2024-04-03 22:53:15 +0100

    flake.lock: Update
    Flake lock file updates:
    
    • Updated input 'deploy-rs':
        'github:serokell/deploy-rs/0a0187794ac7f7a1e62cda3dabf8dc041f868790' (2024-02-16)
      → 'github:serokell/deploy-rs/88b3059b020da69cbe16526b8d639bd5e0b51c8b' (2024-04-01)
    • Updated input 'home-manager':
        'github:nix-community/home-manager/1c2c5e4cabba4c43504ef0f8cc3f3dfa284e2dbb' (2024-03-22)
      → 'github:nix-community/home-manager/80546b220e95a575c66c213af1b09fe255299438' (2024-04-02)
    • Updated input 'nix-index-database':
        'github:nix-community/nix-index-database/34519f3bb678a5abbddf7b200ac5347263ee781b' (2024-03-24)
      → 'github:nix-community/nix-index-database/2844b5f3ad3b478468151bd101370b9d8ef8a3a7' (2024-03-31)
    • Updated input 'nixpkgs':
        'github:KoviRobi/nixpkgs/3b26f68c9ae53e56f5a2e6581653f9a0326769ae' (2024-03-24)
      → 'github:NixOS/nixpkgs/e976fa8f49c35cf28496301a1ef2aa23ad576b56' (2024-04-02)

--- overlays/nom-rebuild.nix
+++ overlays/nom-rebuild.nix
@@ -2,13 +2,12 @@ final: prev:
 {
   nom-rebuild = final.nixos-rebuild.overrideAttrs (old: rec {
     name = "nom-rebuild";
-    path = old.path + (final.lib.makeBinPath [ final.nix-output-monitor ]);
     src = final.substitute {
       src = old.src;
       substitutions = [
         "--replace" "nixos-rebuild" "nom-rebuild"
-        "--replace" "nix-build" "nom-build"
-        "--replace" "nix \"\${flakeFlags[@]}\" build" "nom build \"\${flakeFlags[@]}\""
+        "--replace" "nix-build" "${final.nix-output-monitor}/bin/nom-build"
+        "--replace" "nix \"\${flakeFlags[@]}\" build" "${final.nix-output-monitor}/bin/nom build \"\${flakeFlags[@]}\""
       ];
     };
   });
