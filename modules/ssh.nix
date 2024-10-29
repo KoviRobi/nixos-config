bf996fbbac3a230419c9fe424ec33ba3d36118c8
Kovacsics Robert <kovirobi@gmail.com>
2024-10-29 11:01:04 +0000

    Try using pam no_google_authenticator.users listfile

--- modules/ssh.nix
+++ modules/ssh.nix
@@ -8,17 +8,27 @@
 
 {
   environment.systemPackages = [ pkgs.google-authenticator ];
-  security.pam.makeHomeDir.skelDirectory = "/etc/skel";
-  security.pam.services.sshd.makeHomeDir = true;
-  security.pam.services.sshd.googleAuthenticator.enable = true;
-  users.groups.no-google-authenticator = { };
+
+  security.pam.services = {
+    sshd.googleAuthenticator.enable = true;
+    # Set up a substack to be able to turn it off for users based on a listsfile
+    sshd.rules.auth."no_google_authenticator.users" = {
+      # Skip next (google-authenticator) if it is contained in the file, see
+      # pam.d(3) for more details
+      control = lib.mkForce "[success=1 default=ignore]";
+      modulePath = "${pkgs.linux-pam}/lib/security/pam_listfile.so";
+      settings = {
+        item = "user";
+        file = "/etc/pam/no_google_authenticator.users";
+        sense = "allow";
+        onerr = "fail";
+      };
+      order = config.security.pam.services.sshd.rules.auth.google_authenticator.order - 1;
+    };
+  };
+
   services.openssh = {
     enable = true;
     settings.PermitRootLogin = "no";
-    extraConfig = ''
-      Match Group !no-google-authenticator,*
-        AuthenticationMethods publickey keyboard-interactive
-      Match All
-    '';
   };
 }
