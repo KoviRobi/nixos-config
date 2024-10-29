3605db7e04a5b75b2a0711ae7b09a828bcb00351
Kovacsics Robert <kovirobi@gmail.com>
2024-10-29 16:50:19 +0000

    Add GDB to nvim-dap
    See https://github.com/mfussenegger/nvim-dap/wiki/Debug-Adapter-installation

--- home/neovim/lua/plugins/lsp.lua
+++ home/neovim/lua/plugins/lsp.lua
@@ -41,4 +41,56 @@ return {
 			},
 		},
 	},
+
+	{
+		"mfussenegger/nvim-dap",
+		opts = function()
+			local dap = require("dap")
+			dap.adapters.gdb = {
+				type = "executable",
+				command = "gdb",
+				args = { "--interpreter=dap", "--eval-command", "set print pretty on" },
+			}
+			local c_like = { "c", "cpp" }
+			for _, language in ipairs(c_like) do
+				dap.configurations[language] = {
+					{
+						name = "Launch",
+						type = "gdb",
+						request = "launch",
+						program = function()
+							return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+						end,
+						cwd = "${workspaceFolder}",
+						stopAtBeginningOfMainSubprogram = false,
+					},
+					{
+						name = "Select and attach to process",
+						type = "gdb",
+						request = "attach",
+						program = function()
+							return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+						end,
+						pid = function()
+							local name = vim.fn.input("Executable name (filter): ")
+							return require("dap.utils").pick_process({ filter = name })
+						end,
+						cwd = "${workspaceFolder}",
+					},
+					{
+						name = "Attach to gdbserver",
+						type = "gdb",
+						request = "attach",
+						target = function()
+							return vim.fn.input("Target", ":3333")
+						end,
+						program = function()
+							return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+						end,
+						cwd = "${workspaceFolder}",
+					},
+				}
+			end
+		end,
+	},
 }
