d12cabd05dd6250594a931171783e2176319bf12
Kovacsics Robert <kovirobi@gmail.com>
2024-11-12 12:39:54 +0000

    vim: Configure CCLS to use rootdir containing CMakeLists.txt; use build dir

--- home/neovim/lua/plugins/lsp.lua
+++ home/neovim/lua/plugins/lsp.lua
@@ -1,3 +1,4 @@
+local util = require("lspconfig.util")
 return {
 	{
 		"neovim/nvim-lspconfig",
@@ -5,7 +6,15 @@ return {
 			servers = {
 				nil_ls = { mason = false },
 				lua_ls = { mason = false },
-				ccls = {},
+				ccls = {
+					init_options = {
+						compilationDatabaseDirectory = "build",
+					},
+					root_dir = function(fname)
+						return util.root_pattern("compile_commands.json", ".ccls", "CMakeLists.txt")(fname)
+							or util.find_git_ancestor(fname)
+					end,
+				},
 				ocamllsp = { mason = false },
 				gopls = {},
 				rust_analyzer = {},
