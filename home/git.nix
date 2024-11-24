48a8b1c301597785dfed71b60849456506feff81
Kovacsics Robert <kovirobi@gmail.com>
2024-11-24 22:09:22 +0000

    Git g/lg aliases

--- home/git.nix
+++ home/git.nix
@@ -13,7 +13,8 @@ in
     userName = "Kovacsics Robert";
     userEmail = lib.mkDefault "kovirobi@gmail.com";
     aliases = {
-      lg = "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit";
+      g = "log --oneline --graph";
+      lg = "log --oneline";
       pcc = "push -o merge_request.create -o merge_request.remove_source_branch -o merge_request.assign='robert.kovacsics' -o merge_request.target=master";
       prich = "push -o merge_request.create -o merge_request.remove_source_branch -o merge_request.assign='robert.kovacsics' -o merge_request.target=richmond";
       pgl = "push -o merge_request.create -o merge_request.remove_source_branch -o merge_request.assign='rmk' -o merge_request.target=master";
