a68cfaa9d110ef26bae8fe4cb0fa5d042a53b8af
Robert Kovacsics <robertkovacsics@carallon.com>
2025-01-20 15:29:39 +0000

    Add .{bash,zsh}rc.local files for OpenSUSE

--- home/shell.nix
+++ home/shell.nix
@@ -17,10 +17,6 @@
       carapace
     ];
 
-  programs.bash.initExtra = ''
-    eval "$(${pkgs.zoxide}/bin/zoxide init bash | ${pkgs.gnused}/bin/sed 's|\\command zoxide|\\command ${pkgs.zoxide}/bin/zoxide|g')"
-  '';
-
   home.sessionVariables = {
     EDITOR = "nvim";
     PAGER = "less";
@@ -98,69 +94,84 @@
     jtl = "journalctl";
   };
 
-  programs.bash.enable = true;
+  programs = {
+    bash = {
+      enable = true;
+      initExtra = ''
+        eval "$(${pkgs.zoxide}/bin/zoxide init bash | ${pkgs.gnused}/bin/sed 's|\\command zoxide|\\command ${pkgs.zoxide}/bin/zoxide|g')"
 
-  programs.zsh = {
-    enable = true;
-    initExtra = ''
-      unsetopt beep
+        if [ -e "$HOME/.bashrc.local" ]; then
+          source "$HOME/.bashrc.local"
+        fi
+      '';
+    };
 
-      export ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE=fg=yellow
-      export VERSION_CONTROL=numbered
+    zsh = {
+      enable = true;
+      initExtra = ''
+        unsetopt beep
 
-
-      bindkey -e
-      autoload edit-command-line
-      zle -N edit-command-line
-      bindkey "^X^E" edit-command-line
-      bindkey "^P" up-history
-      bindkey "^N" down-history
-      bindkey "^W" kill-region
-      bindkey "^[u" up-case-word
-      bindkey "^[l" down-case-word
-      bindkey "^Q" push-line
-      bindkey "^Z" undo
-
-      [ -z "''${terminfo[kcbt]}" ]  || bindkey "''${terminfo[kcbt]}"  reverse-menu-complete
-      [ -z "''${terminfo[kdch1]}" ] || bindkey "''${terminfo[kdch1]}" delete-char
-      [ -z "''${terminfo[kich1]}" ] || bindkey "''${terminfo[kich1]}" overwrite-mode
-      [ -z "''${terminfo[khome]}" ] || bindkey "''${terminfo[khome]}" beginning-of-line
-      [ -z "''${terminfo[kend]}" ]  || bindkey "''${terminfo[kend]}"  end-of-line
-      bindkey "^[[1;5C" forward-word
-      bindkey "^[[1;5D" backward-word
-
-      # Often I do want to go back to underscores or hyphens
-      WORDCHARS=""
-      eval $(${pkgs.thefuck}/bin/thefuck --alias fck)
+        export ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE=fg=yellow
+        export VERSION_CONTROL=numbered
 
 
-      # The following lines were added by compinstall
-      zstyle ':completion:*' completer _complete _ignored
-      zstyle ':completion:*' group-name ""
-      zstyle ':completion:*' insert-unambiguous true
-      zstyle ':completion:*' list-colors ''${(s.:.)LS_COLORS}
-      zstyle ':completion:*' matcher-list 'm:{[:lower:]}={[:upper:]}' 'r:|[._-]=** r:|=**'
-      zstyle ':completion:*' menu select
-      zstyle :compinstall filename '/home/rmk/.zsh.comp'
+        bindkey -e
+        autoload edit-command-line
+        zle -N edit-command-line
+        bindkey "^X^E" edit-command-line
+        bindkey "^P" up-history
+        bindkey "^N" down-history
+        bindkey "^W" kill-region
+        bindkey "^[u" up-case-word
+        bindkey "^[l" down-case-word
+        bindkey "^Q" push-line
+        bindkey "^Z" undo
 
-      autoload -Uz compinit
-      compinit
-      # End of lines added by compinstall
-      compdef _nixos-rebuild nom-rebuild
-      compdef _man viman
-      unsetopt flow_control
-      setopt AUTO_PUSHD
-      source ${pkgs.oh-my-zsh}/share/oh-my-zsh/plugins/history-substring-search/history-substring-search.plugin.zsh
-      export HISTORY_SUBSTRING_SEARCH_ENSURE_UNIQUE=1
+        [ -z "''${terminfo[kcbt]}" ]  || bindkey "''${terminfo[kcbt]}"  reverse-menu-complete
+        [ -z "''${terminfo[kdch1]}" ] || bindkey "''${terminfo[kdch1]}" delete-char
+        [ -z "''${terminfo[kich1]}" ] || bindkey "''${terminfo[kich1]}" overwrite-mode
+        [ -z "''${terminfo[khome]}" ] || bindkey "''${terminfo[khome]}" beginning-of-line
+        [ -z "''${terminfo[kend]}" ]  || bindkey "''${terminfo[kend]}"  end-of-line
+        bindkey "^[[1;5C" forward-word
+        bindkey "^[[1;5D" backward-word
 
-      eval "$(${pkgs.zoxide}/bin/zoxide init zsh | ${pkgs.gnused}/bin/sed -e 's|\\command zoxide|\\command ${pkgs.zoxide}/bin/zoxide|g' -e '/compdef/d')"
+        # Often I do want to go back to underscores or hyphens
+        WORDCHARS=""
+        eval $(${pkgs.thefuck}/bin/thefuck --alias fck)
 
-      ${pkgs.fortune}/bin/fortune ${pkgs.apf-cookie}/share/games/fortunes/apf-cookie
-    '';
 
-    enableCompletion = true;
-    enableVteIntegration = true;
-    autosuggestion.enable = true;
-    syntaxHighlighting.enable = true;
+        # The following lines were added by compinstall
+        zstyle ':completion:*' completer _complete _ignored
+        zstyle ':completion:*' group-name ""
+        zstyle ':completion:*' insert-unambiguous true
+        zstyle ':completion:*' list-colors ''${(s.:.)LS_COLORS}
+        zstyle ':completion:*' matcher-list 'm:{[:lower:]}={[:upper:]}' 'r:|[._-]=** r:|=**'
+        zstyle ':completion:*' menu select
+        zstyle :compinstall filename '/home/rmk/.zsh.comp'
+
+        autoload -Uz compinit
+        compinit
+        # End of lines added by compinstall
+        compdef _nixos-rebuild nom-rebuild
+        compdef _man viman
+        unsetopt flow_control
+        setopt AUTO_PUSHD
+        source ${pkgs.oh-my-zsh}/share/oh-my-zsh/plugins/history-substring-search/history-substring-search.plugin.zsh
+        export HISTORY_SUBSTRING_SEARCH_ENSURE_UNIQUE=1
+
+        eval "$(${pkgs.zoxide}/bin/zoxide init zsh | ${pkgs.gnused}/bin/sed -e 's|\\command zoxide|\\command ${pkgs.zoxide}/bin/zoxide|g' -e '/compdef/d')"
+
+        ${pkgs.fortune}/bin/fortune ${pkgs.apf-cookie}/share/games/fortunes/apf-cookie
+
+        if [ -e "$HOME/.zshrc.local" ]; then
+          source "$HOME/.zshrc.local"
+        fi
+      '';
+
+      enableCompletion = true;
+      enableVteIntegration = true;
+      autosuggestion.enable = true;
+      syntaxHighlighting.enable = true;
+    };
   };
 }
