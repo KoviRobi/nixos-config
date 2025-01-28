23183d76d7d54e95f13280e30db7ff2fff72afc9
Kovacsics Robert <kovirobi@gmail.com>
2025-01-28 23:35:59 +0000

    Fix SSH pubkey

--- pubkeys.nix
+++ pubkeys.nix
@@ -1,7 +1,7 @@
 {
   "pc-nixos-a" = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBxS1hIoi4jj4h00KoIfBJJX6aMF5TtdZZxBOqLRRKCH rmk35 pc id_ed25519";
   "rmk-legion" = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPHT7UP7FsIUXpJGhUijpm3uAw99jAJis8vzdSJT3IWB u0_a345@Legion Duel";
-  "rmk-legion-2" = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBXOwVYwFZYySvnShFyyehkdt5ANRQ64re9Fzm5xY8q PS@Legion Duel";
+  "rmk-legion-2" = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBXOwVYwFZYySvnShFyyehkdt5ANRQ64re9Fzm5xY8q9 PS@Legion Duel";
   "acer-nixos-a" = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICf3X2PMIpVFKstmrMhvnfHRrGKIzH8F4/I4xgHg2zB1 rmk@hp-nixos-a";
   "promethium-nixos-a" = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILPZ0IUFFBr4jQtm91e2YiAnQwZSTfpKFukeRN2oZH2J rmk@promethium-nixos-a";
   "c930" = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILDlCJVI7R2rTB0uVGHy+RRFM31wjYO0vIKwM0syi0rT rmk@c930";
