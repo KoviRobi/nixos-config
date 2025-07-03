e3940b07f3ac1e4d601e811683251003a38108d9
Robert Kovacsics <kovirobi@gmail.com>
2025-07-04 00:26:06 +0100

    Fix home-manager overlay issue
    It didn't know to pass pkgs and config so it didn't, hence the need for
    extraSpecialArgs.

--- home/home-manager-only.nix
+++ home/home-manager-only.nix
@@ -1,7 +1,7 @@
-args: {
+{pkgs, config, ...}: {
   imports = [
     ./default.nix
   ];
 
-  home.packages = import ../packages/base.nix args;
+  home.packages = import ../packages/base.nix {inherit pkgs config;};
 }
