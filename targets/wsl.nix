999650afdbb74f19f0477eef7f0e44056c251f77
Kovacsics Robert <kovirobi@gmail.com>
2025-08-13 21:09:26 +0000

    wsl: Disable picom (wslg has its own compositor)

--- targets/wsl.nix
+++ targets/wsl.nix
@@ -88,6 +88,7 @@
   home-manager.users.default-user = {
     services.gnome-keyring.enable = true;
 
+    services.picom.enable = lib.mkForce false;
     services.feh-random-background.enable = lib.mkForce false;
     services.udiskie.enable = lib.mkForce false;
     systemd.user.services.setxkbmap.Install.WantedBy = lib.mkForce [ ];
