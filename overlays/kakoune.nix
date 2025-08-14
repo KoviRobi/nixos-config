55b46a13a091a8139092429f0fdb7967b7c2c671
Kovacsics Robert <kovirobi@gmail.com>
2025-08-14 20:30:38 +0000

    kakoune: Make search highlight more obvious

--- overlays/kakoune.nix
+++ overlays/kakoune.nix
@@ -159,7 +159,7 @@ final: prev: {
 
       add-highlighter global/ wrap -word -marker ⏎
       add-highlighter global/ show-matching
-      add-highlighter global/highlight-search dynregex '%reg{/}' 0:+i
+      add-highlighter global/highlight-search dynregex '%reg{/}' 0:,rgba:80800040+i
       add-highlighter global/show-trailing-whitespaces regex '\h+$' 0:,,red+u
       add-highlighter global/show-inconsistent-tabs-1 regex '( +)(\t+)' 2:,,red+u
       add-highlighter global/show-inconsistent-tabs-2 regex '(\t+)( +)' 1:,,red+u
