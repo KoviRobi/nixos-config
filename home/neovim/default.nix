21c8160f71568cbc6ef15d3026e787fb9014d357
Kovacsics Robert <kovirobi@gmail.com>
2025-10-03 15:48:47 +0100

    Add gruvbox vim theme

--- home/neovim/default.nix
+++ home/neovim/default.nix
@@ -1,6 +1,7 @@
 {
   config,
   lib,
+  pkgs,
   ...
 }:
 with lib;
@@ -19,6 +20,24 @@ in
       vimAlias = true;
       vimdiffAlias = true;
       withNodeJs = true;
+      plugins = [ pkgs.vimPlugins.gruvbox ];
+      extraLuaConfig = ''
+        local brightness = "light"
+        local fpath = vim.fs.dirname(vim.fn.stdpath("state")) .. "/brightness"
+        local fp = io.open(fpath, "r")
+        if fp ~= nil then
+          local fread = fp:read():gsub("^%s+", ""):gsub("%s+$", "")
+          if fread == "light" or fread == "dark" then
+            brightness = fread
+          end
+        end
+        vim.o.background = brightness
+        vim.api.nvim_create_autocmd({ "VimEnter" }, {
+          callback = function()
+            vim.cmd.colorscheme("gruvbox")
+          end,
+        })
+      '';
     };
   };
 }
