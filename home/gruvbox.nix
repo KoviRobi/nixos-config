d7658cf737c2bfb8983edd74df9a65a2096c726c
Kovacsics Robert <kovirobi@gmail.com>
2025-10-03 20:10:13 +0100

    Remove X11 solarized config

--- home/gruvbox.nix
+++ home/gruvbox.nix
@@ -23,17 +23,6 @@ in
   };
 
   config = {
-    xresources.extraConfig = ''
-      #include "${
-        pkgs.fetchFromGitHub {
-          owner = "solarized";
-          repo = "xresources";
-          rev = "025ceddbddf55f2eb4ab40b05889148aab9699fc";
-          sha256 = "0lxv37gmh38y9d3l8nbnsm1mskcv10g3i83j0kac0a2qmypv1k9f";
-        }
-      }/Xresources.${cfg.brightness}"
-    '';
-
     home.pointerCursor = {
       package = pkgs.capitaine-cursors-themed;
       name = "Capitaine Cursors (Gruvbox)";
