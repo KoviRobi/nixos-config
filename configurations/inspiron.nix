4a6b3c3d568dca8cfacdeec519bd135be675d057
Kovacsics Robert <kovirobi@gmail.com>
2025-10-14 23:10:58 +0100

    Add virtualisation to inspiron

--- configurations/inspiron.nix
+++ configurations/inspiron.nix
@@ -9,6 +9,27 @@
     (import ../modules/avahi.nix { publish = false; })
   ];
 
+  virtualisation = {
+    docker.enable = true;
+    podman.enable = true;
+
+    libvirtd = {
+      enable = true;
+      nss.enableGuest = true;
+      qemu = {
+        vhostUserPackages = [ pkgs.virtiofsd ];
+        swtpm = {
+          enable = true;
+        };
+      };
+    };
+  };
+
+  users.users.default-user.extraGroups = [
+    "docker"
+    "libvirtd"
+  ];
+
   services = {
     xserver.dpi = 109;
     desktopManager.gnome.enable = true;
