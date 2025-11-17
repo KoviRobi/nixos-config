a9a97447c525ba58cda2613c20eb5a5d7084d83f
Kovacsics Robert <kovirobi@gmail.com>
2025-11-17 12:42:00 +0000

    nit: Fix space error

--- home/shell.nix
+++ home/shell.nix
@@ -265,7 +265,7 @@
       nf = "n flake";
       nepl =
         ''nix repl --expr "let flake = builtins.getFlake \"nixos-config\"; in { inherit flake; }''
-        + lib.optionalString ( config ? "nixos") ''// flake.nixosConfigurations.${config.nixos.hostName}''
+        + lib.optionalString (config ? "nixos") ''// flake.nixosConfigurations.${config.nixos.hostName}''
         + ''"'';
 
       dea = "direnv allow";
