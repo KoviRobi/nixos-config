562b51e78df39166477904ac5cbb31c95b1b01b7
Kovacsics Robert <kovirobi@gmail.com>
2025-12-04 09:59:26 +0000

    Add new SSH key

--- pubkeys.nix
+++ pubkeys.nix
@@ -1,11 +1,22 @@
 {
-  "pc-nixos-b" = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINKljFBX/jPYEOhF3WQaZ34gDvnU7PVlMrCe7GCUOqFf rmk@pc-nixos-b";
-  "rmk-legion" = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPHT7UP7FsIUXpJGhUijpm3uAw99jAJis8vzdSJT3IWB u0_a345@Legion Duel";
-  "rmk-legion-2" = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBXOwVYwFZYySvnShFyyehkdt5ANRQ64re9Fzm5xY8q9 PS@Legion Duel";
-  "acer-nixos-a" = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICf3X2PMIpVFKstmrMhvnfHRrGKIzH8F4/I4xgHg2zB1 rmk@hp-nixos-a";
-  "promethium-nixos-a" = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILPZ0IUFFBr4jQtm91e2YiAnQwZSTfpKFukeRN2oZH2J rmk@promethium-nixos-a";
-  "c930" = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILDlCJVI7R2rTB0uVGHy+RRFM31wjYO0vIKwM0syi0rT rmk@c930";
-  "roberty-pi" = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPMz0UH47Tx1LIC+5CG2S7ieSbCBCyusBMEJ697oDK9X rmk@roberty-pi";
-  "inspiron-wsl" = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPMBEHMlGQvQ5z9tR0k5S6BQmt43/RedrPpAslssSetY rmk@inspiron-wsl";
-  "inspiron" = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJU+5tlDf7zyFTr1Hjptm/j0OZD0NrJRDO0OzM9zOBVj rmk@inspiron";
+  "pc-nixos-b" =
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINKljFBX/jPYEOhF3WQaZ34gDvnU7PVlMrCe7GCUOqFf rmk@pc-nixos-b";
+  "rmk-legion" =
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPHT7UP7FsIUXpJGhUijpm3uAw99jAJis8vzdSJT3IWB u0_a345@Legion Duel";
+  "rmk-legion-2" =
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBXOwVYwFZYySvnShFyyehkdt5ANRQ64re9Fzm5xY8q9 PS@Legion Duel";
+  "acer-nixos-a" =
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICf3X2PMIpVFKstmrMhvnfHRrGKIzH8F4/I4xgHg2zB1 rmk@hp-nixos-a";
+  "promethium-nixos-a" =
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILPZ0IUFFBr4jQtm91e2YiAnQwZSTfpKFukeRN2oZH2J rmk@promethium-nixos-a";
+  "c930" =
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILDlCJVI7R2rTB0uVGHy+RRFM31wjYO0vIKwM0syi0rT rmk@c930";
+  "roberty-pi" =
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPMz0UH47Tx1LIC+5CG2S7ieSbCBCyusBMEJ697oDK9X rmk@roberty-pi";
+  "inspiron-wsl" =
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPMBEHMlGQvQ5z9tR0k5S6BQmt43/RedrPpAslssSetY rmk@inspiron-wsl";
+  "inspiron" =
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJU+5tlDf7zyFTr1Hjptm/j0OZD0NrJRDO0OzM9zOBVj rmk@inspiron";
+  "rmk-pix9a" =
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBQr9Ug3wG/KgLvOiRP9O5dhU/bR8Qf08+cjn7hopfU6 rmk-pix9a";
 }
