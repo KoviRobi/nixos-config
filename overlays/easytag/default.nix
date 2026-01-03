4fd52faaaa3dbf875864f512c224d03b4b4848d6
Kovacsics Robert <kovirobi@gmail.com>
2026-01-03 15:23:09 +0000

    easytag: Use upstream patch for pointer-compare

--- overlays/easytag/default.nix
+++ overlays/easytag/default.nix
@@ -3,7 +3,6 @@ final: prev: {
     CFLAGS =
       old.CFLAGS or ""
       + " -std=c11"
-      + " -Wno-error=pointer-compare"
       + " -Wno-error=cast-function-type"
       + " -Wno-error=implicit-function-declaration"
       + " -Wno-error=array-bounds"
@@ -13,6 +12,10 @@ final: prev: {
     patches = old.patches or [ ] ++ [
       ./0001-Fix-missing-strings.h-providing-strcasecmp.patch
       ./0002-Fix-missing-const-qualifier.patch
+      (final.fetchpatch {
+        url = "https://gitlab.gnome.org/GNOME/easytag/-/commit/d0c05132e6e32f907044fbe5ebd4fe90888b5010.patch";
+        hash = "sha256-pr3enGztBn+uDUFO2S43utCWNxUQ0J5AMS72vasSlgc=";
+      })
     ];
   });
 }
