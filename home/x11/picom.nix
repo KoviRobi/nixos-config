66eee1591d86c38cf5b998d4e3a6ec703ac5062b
Kovacsics Robert <kovirobi@gmail.com>
2026-02-12 19:48:32 +0000

    Do transparent clipping
    I only care about transparency for the background

--- home/x11/picom.nix
+++ home/x11/picom.nix
@@ -11,13 +11,13 @@
       { match = "INVERT@ = 1";                                          invert-color = true;  },
       { match = "INVERT@ = 0";                                          invert-color = false; },
 
-      { match = "!_NET_WM_WINDOW_OPACITY && class_i = 'ringboard-egui'";         opacity = 0.87; },
-      { match = "!_NET_WM_WINDOW_OPACITY && class_i %= 'scratch_*'";             opacity = 0.87; },
-      { match = "!_NET_WM_WINDOW_OPACITY && class_i = 'st-256color'";            opacity = 0.91; },
-      { match = "!_NET_WM_WINDOW_OPACITY && class_i = 'ghostty'";                opacity = 0.91; },
-      { match = "!_NET_WM_WINDOW_OPACITY && class_i = 'org.wezfurlong.wezterm'"; opacity = 0.91; },
-      { match = "_NET_WM_STATE@[*] = '_NET_WM_STATE_HIDDEN'";                    opacity = 0;    },
-      { match = "class_i = 'i3lock'";                                            opacity = 1;    },
+      { match = "!_NET_WM_WINDOW_OPACITY && class_i = 'ringboard-egui'";         opacity = 0.87; transparent-clipping = true; },
+      { match = "!_NET_WM_WINDOW_OPACITY && class_i %= 'scratch_*'";             opacity = 0.87; transparent-clipping = true; },
+      { match = "!_NET_WM_WINDOW_OPACITY && class_i = 'st-256color'";            opacity = 0.91; transparent-clipping = true; },
+      { match = "!_NET_WM_WINDOW_OPACITY && class_i = 'ghostty'";                opacity = 0.91; transparent-clipping = true; },
+      { match = "!_NET_WM_WINDOW_OPACITY && class_i = 'org.wezfurlong.wezterm'"; opacity = 0.91; transparent-clipping = true; },
+      { match = "_NET_WM_STATE@[*] = '_NET_WM_STATE_HIDDEN'";                    opacity = 0;                                 },
+      { match = "class_i = 'i3lock'";                                            opacity = 1;    transparent-clipping = true; },
     )
   '';
 }
