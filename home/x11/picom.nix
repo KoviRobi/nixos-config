c564e7cf5e9aacf862e76008a3e52f661930e962
Kovacsics Robert <kovirobi@gmail.com>
2026-02-13 11:56:29 +0000

    picom: Fix i3lock transparent-clipping

--- home/x11/picom.nix
+++ home/x11/picom.nix
@@ -20,7 +20,10 @@
       { match = "!_NET_WM_WINDOW_OPACITY && class_i = 'ghostty'";                opacity = 0.91; },
       { match = "!_NET_WM_WINDOW_OPACITY && class_i = 'org.wezfurlong.wezterm'"; opacity = 0.91; },
       { match = "_NET_WM_STATE@[*] = '_NET_WM_STATE_HIDDEN'";                    opacity = 0;    },
-      { match = "class_i = 'i3lock'";                                            opacity = 0.4;  },
+
+      # Ensure it only shows the background (needs to have transparent-clipping set
+      # explicitly for some reason)
+      { match = "class_i = 'i3lock'"; transparent-clipping = true; opacity = 0.4;  },
     )
   '';
 }
