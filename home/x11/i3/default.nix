95ad587013e6af7a1ad049f28171404f18792cf6
Kovacsics Robert <kovirobi@gmail.com>
2026-05-06 17:21:23 +0100

    Also allow shortcuts inhibit on virt-manager

--- home/x11/i3/default.nix
+++ home/x11/i3/default.nix
@@ -233,7 +233,9 @@ in
       for_window [app_id="foot"] border pixel ${toString border}
       for_window [app_id="^scratch_.*$"] border pixel ${toString border}
       for_window [app_id="^termfilechooser$"] floating enable
+
       for_window [app_id="^wlvncc$"] shortcuts_inhibitor enable
+      for_window [app_id="^.virt-manager-wrapped$"] shortcuts_inhibitor enable
 
       input type:touchpad tap_button_map lrm
       input type:touchpad tap enabled
