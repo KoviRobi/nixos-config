69e682bc669f75ae32e3f8fdeb9f451729ae0c00
Kovacsics Robert <kovirobi@gmail.com>
2026-06-27 22:32:14 +0100

    Add osc7-spawn script

---
+++ packages/osc7-spawn.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+osc7() {
+  # shellcheck disable=SC1003 # ASNI ST escape
+  LC_ALL=C printf '\e]7;file://%s%s\e\\' "${HOSTNAME}" "${PWD}"
+}
+
+OSC7=${OSC7:-$(osc7)}
+
+# Local OSC7 handled by foot already
+if [ -n "${OSC7##file://"${HOSTNAME}"/*}" ]; then
+  #
+  # shellcheck disable=SC1003 # ASNI ST escape
+  LC_ALL=C printf '\e]7;%s\e\\' "${OSC7}"
+  # Trusted hosts
+  for host in promethium-nix1 inspiron kovirobi.uk; do
+    if [ -z "${OSC7##file://"${host}"/*}" ]; then
+      echo "${OSC7#file://"${host}"}"
+      exec ssh -t "${host}" \
+        "cd '${OSC7#file://"${host}"}'; \
+         exec \"\$SHELL\" --login -c 'exec zms'"
+    fi
+  done
+fi
+
+ZMX_EXEC=1 zms "$@" || ( osc7; exec "$SHELL" "$@" )
---
+++ packages/zms.sh
@@ -0,0 +1,57 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+osc7() {
+  # shellcheck disable=SC1003 # ASNI ST escape
+  LC_ALL=C printf '\e]7;file://%s%s\e\\' "${HOSTNAME}" "${PWD}"
+}
+
+zmx-select() {
+  local display
+  display=$( ( zmx list 2>/dev/null || true ) | \
+      while IFS=$'\t' read -r name pid clients _created dir; do
+    name=${name#*name=}
+    pid=${pid#pid=}
+    clients=${clients#clients=}
+    dir=${dir#started_in=}
+    printf "%s\tpid:%s\tclients:%s\t%s\n" "$name" "$pid" "$clients" "$dir"
+  done | sort -t$'\t' -k3.9n,4 -k1,1)
+
+  local output query key name
+  # shellcheck disable=SC2016 # Expanded in fzf
+  output=$({ [[ -n "$display" ]] && echo "$display"; } | fzf \
+    --print-query \
+    --expect=ctrl-d \
+    --bind $'tab:transform-query:echo "${${FZF_CURRENT_ITEM}/\t*/}"' \
+    --bind 'load:transform:column -t' \
+    --reverse \
+    --prompt="zmx> " \
+    --header="Enter: accept | Tab: complete | Ctrl-D: z and create new" \
+    --preview=$'zmx history "${${FZF_CURRENT_ITEM}/\t*/}"' \
+    --preview-window=right:60%:follow || true
+  )
+  local rc=$?
+
+  query=$(echo "$output" | sed -n '1p')
+  key=$(echo "$output" | sed -n '2p')
+  selected=$(echo "$output" | sed -n '3p')
+
+  if [[ "$key" == "ctrl-d" && -n "$query" ]]; then
+    # zoxide and spawn
+    name="$query"
+    cd "$(zoxide query "$name")" || true
+    osc7
+    ${ZMX_EXEC:+exec} zmx attach "$name"
+  elif [[ -n "$query" ]]; then
+    name="$query"
+    ${ZMX_EXEC:+exec} zmx attach "$name"
+  elif [[ $rc -eq 0 ]]; then
+    name=${selected/	*/}
+    ${ZMX_EXEC:+exec} zmx attach "$name"
+  else
+    return 130
+  fi
+}
+
+zmx-select "$@"
