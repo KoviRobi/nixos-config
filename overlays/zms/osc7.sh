00eb8b875d3073cb24541d9d27c0424b152877a8
Kovacsics Robert <kovirobi@gmail.com>
2026-08-17 16:49:48 +0100

    osc7: Fall back if HOSTNAME/PWD is not set
    For some reason it seems like HOSTNAME isn't set after SSHing

--- overlays/zms/osc7.sh
+++ overlays/zms/osc7.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 
 osc7() {
   # shellcheck disable=SC1003 # ASNI ST escape
-  LC_ALL=C printf '\e]7;file://%s%s\e\\' "${HOSTNAME}" "${PWD}"
+  LC_ALL=C printf '\e]7;file://%s%s\e\\' "${HOSTNAME:-$(hostname)}" "${PWD:-$(pwd)}"
 }
 
 osc7
